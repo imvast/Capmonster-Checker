a65220dc9e76c3051863743c7ed277a59790c497
chelfish <97201007+chelfish@users.noreply.github.com>
2023-02-11 21:58:40 -0600

    fixed limit parameter passed to asyncio.run

--- main.py
+++ main.py
@@ -14,33 +14,33 @@ class CapMonsterChecker:
     def __init__(self):
         with open("keys.txt", 'r') as f:
             self.keys = [line.strip() for line in f.readlines()]
-    
+
     async def check_single_key(self, key):
         try:
-            checkResp = await httpx.post(
-                "https://api.capmonster.cloud/getBalance",
-                json = { "clientKey": key }
-            )
-            if checkResp.status_code == 200:
-                balance = checkResp.json().get('balance')
-                if balance is not None:
-                    print(f"[+] Valid Key: {key} | Balance: {balance}")
-            elif checkResp.json().get('errorCode') == "ERROR_KEY_DOES_NOT_EXIST":
-                print(f"[-] Invalid Key: {key}")
-            else:
-                print(f"[!] Error checking key: {key} | {checkResp.status_code} | {checkResp.json()}")
+            async with httpx.AsyncClient() as client:
+                checkResp = await client.post(
+                    "https://api.capmonster.cloud/getBalance",
+                    json={"clientKey": key}
+                )
+                if checkResp.status_code == 200:
+                    balance = checkResp.json().get('balance')
+                    if balance is not None:
+                        print(f"[+] Valid Key: {key} | Balance: {balance}")
+                elif checkResp.json().get('errorCode') == "ERROR_KEY_DOES_NOT_EXIST":
+                    print(f"[-] Invalid Key: {key}")
+                else:
+                    print(f"[!] Error checking key: {key} | {checkResp.status_code} | {checkResp.json()}")
         except Exception as e:
             print(f"[ERROR] Exception in checker -> {e}")
 
-    async def Check(self):
+    async def check(self):
         print(f"[*] Starting checker with {len(self.keys)} keys...")
         tasks = [self.check_single_key(key) for key in self.keys]
         await asyncio.gather(*tasks)
-                
 
 if __name__ == "__main__":
     try:
         num_threads = int(input("Amount of Threads: "))
-        asyncio.run(CapMonsterChecker().Check(), limit=num_threads)
+        asyncio.run(CapMonsterChecker().check())
     except KeyboardInterrupt:
         os._exit(0)
